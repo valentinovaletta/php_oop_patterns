cbbc4feb52f54840957236e7893ff9d9e4e12cab
vasiljevalentin@yandex.ru <vasiljevalentin@yandex.ru>
2025-07-16 15:38:00 +0300

    add message menu

--- project/controllers/TelegramController.php
+++ project/controllers/TelegramController.php
@@ -1,30 +1,33 @@
 <?php
-	namespace Project\Controllers;
-	use \Core\Controller;
-	use \Project\Classes\TelegramApi\TelegramSDK;
-	use \Project\Classes\MessageFactory\MessageFactory;
 
-	class TelegramController extends Controller
+namespace Project\Controllers;
+
+use \Core\Controller;
+use \Project\Classes\TelegramApi\TelegramSDK;
+use \Project\Classes\MessageFactory\MessageFactory;
+
+class TelegramController extends Controller
+{
+	public function index($params = [])
 	{
-		public function index($params = []) {
 
-			if ($params['var1'] != trim(getenv('TELEGRAM_BOT_CODE'))) {
-				return $this->render('telegram/error', array_merge($params, ['error' => 'incorrect telegram auth code']));
-			}
-			if (!isset($params['message']['chat']['id']) && !isset($params['message']['text'])) {
-				return $this->render('telegram/error', array_merge($params, ['error' => 'undefined telegram chat id or telegram message']));
-			}
-						
-			// Here is an abstract method pattern that creates classes according to a message that we receive from telegram bot.
-			// You can add as many messages as you want. Just add a message file into "project/messagefactory/messages" folder.
-			// Message file must have name starting with "Message" plus command that telegram bot can send us.
-			// Commands that starts from / must follow "Message_command" pattern in the name. 
-			$ms = new MessageFactory($params);
-			$msObject = $ms->create();
-
-			$tg = new TelegramSDK(getenv('TELEGRAM_BOT_TOKEN'));
-			$tgMessage = $tg->sendMessage('sendMessage', ['chat_id' => $msObject->getChatId(), 'text' => $msObject->getMessage(), 'reply_markup' => json_encode(["keyboard" => [["/start", "/test", "/hello"]],"resize_keyboard" => true,"one_time_keyboard" => true])]);
-
-			return $this->render('telegram/index', $params = ['tgMessage' => $tgMessage, 'message' => $msObject]);
+		if ($params['var1'] != trim(getenv('TELEGRAM_BOT_CODE'))) {
+			return $this->render('telegram/error', array_merge($params, ['error' => 'incorrect telegram auth code']));
 		}
+		if (!isset($params['message']['chat']['id']) && !isset($params['message']['text'])) {
+			return $this->render('telegram/error', array_merge($params, ['error' => 'undefined telegram chat id or telegram message']));
+		}
+
+		// Here is an abstract method pattern that creates classes according to a message that we receive from telegram bot.
+		// You can add as many messages as you want. Just add a message file into "project/messagefactory/messages" folder.
+		// Message file must have name starting with "Message" plus command that telegram bot can send us.
+		// Commands that starts from / must follow "Message_command" pattern in the name. 
+		$ms = new MessageFactory($params);
+		$msObject = $ms->create();
+
+		$tg = new TelegramSDK(getenv('TELEGRAM_BOT_TOKEN'));
+		$tgMessage = $tg->sendMessage('sendMessage', ['chat_id' => $msObject->getChatId(), 'text' => $msObject->getMessage(), 'reply_markup' => json_encode(["inline_keyboard" => [[["text" => "button", "callback_data" => "button"]]], "resize_keyboard" => true, "one_time_keyboard" => true])]);
+
+		return $this->render('telegram/index', $params = ['tgMessage' => $tgMessage, 'message' => $msObject]);
 	}
+}
